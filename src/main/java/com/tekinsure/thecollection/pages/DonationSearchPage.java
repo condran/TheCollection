0e04e9288023d36a9735e3dfebec5c5a0ddc9e51
Paul Condran <paul@paulcondran.com>
2012-10-24 20:41:36 +1100

    Basic results table
    Basic database for results
    
    Signed-off-by: Paul Condran <paul@paulcondran.com>

--- src/main/java/com/tekinsure/thecollection/pages/DonationSearchPage.java
+++ src/main/java/com/tekinsure/thecollection/pages/DonationSearchPage.java
@@ -3,12 +3,21 @@ package com.tekinsure.thecollection.pages;
 import com.tekinsure.thecollection.data.CollectionDatabase;
 import com.tekinsure.thecollection.model.data.Donation;
 import com.tekinsure.thecollection.model.ui.DonationSearch;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.repeater.data.GridView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -18,11 +27,16 @@ public class DonationSearchPage extends BasePage {
 
     private DonationSearch donationSearch = new DonationSearch();
 
+    private List<Donation> searchResults = null;
+
+    private DataTable dataTable;
+
     public DonationSearchPage() {
 
         setupUserInterfaceFields();
 
-        recentDonations();
+        searchResults = recentDonations();
+        setupResultsTable();
 
     }
 
@@ -55,5 +69,41 @@ public class DonationSearchPage extends BasePage {
         }
 
         return donationList;
+    }
+
+
+    private void setupResultsTable() {
+
+        List<IColumn> columns = new ArrayList<IColumn>();
+
+        columns.add(new PropertyColumn(new Model<String>("Name"), "name", "name"));
+        columns.add(new PropertyColumn(new Model<String>("Member"), "memberID", "memberID"));
+        columns.add(new PropertyColumn(new Model<String>("Record No"), "receiptNo", "receiptNo"));
+        columns.add(new PropertyColumn(new Model<String>("Total"), "total", "total"));
+
+        SortableDataProvider dataProvider = new SortableDataProvider() {
+            @Override
+            public Iterator iterator(long l, long l1) {
+                List newList = new ArrayList();
+                for (long i = l; i < l1; i++) {
+                    newList.add(searchResults.get((int)i));
+                }
+                return newList.iterator();
+            }
+
+            @Override
+            public long size() {
+                return searchResults.size();
+            }
+
+            @Override
+            public IModel model(Object o) {
+                return new Model((Donation)o);
+            }
+        };
+
+        dataTable = new DefaultDataTable("searchResults", columns, dataProvider, 10);
+        add(dataTable);
+
     }
 }
