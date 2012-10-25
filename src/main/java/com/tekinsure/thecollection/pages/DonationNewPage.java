c1f36571a46af8af071f52c777983e81d4414018
Paul Condran <paul@paulcondran.com>
2012-10-25 22:00:11 +1100

    Starting on Categories
    Signed-off-by: Paul Condran <paul@paulcondran.com>

--- src/main/java/com/tekinsure/thecollection/pages/DonationNewPage.java
+++ src/main/java/com/tekinsure/thecollection/pages/DonationNewPage.java
@@ -1,15 +1,22 @@
 package com.tekinsure.thecollection.pages;
 
 import com.tekinsure.thecollection.data.CollectionDatabase;
+import com.tekinsure.thecollection.model.data.Category;
 import com.tekinsure.thecollection.model.data.Donation;
+import com.tekinsure.thecollection.model.data.DonationCategory;
 import com.tekinsure.thecollection.model.ui.DonationNew;
 import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.PropertyModel;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The new donation page controller. Handles creating and updating new donations.
@@ -20,22 +27,14 @@ public class DonationNewPage extends BasePage {
 
     private DonationNew donationNew = new DonationNew();
     private Form form;
+    private List<Category> availableCategories = new ArrayList<Category>();
 
     public DonationNewPage() {
 
         setupUserInterfaceFields();
 
-        Button saveButton = new Button("save") {
-            @Override
-            public void onSubmit() {
-                CollectionDatabase db =  CollectionDatabase.getInstance();
+        listCategories();
 
-                db.persist(donationNew.getDonation());
-
-                getRequestCycle().setResponsePage(DonationSearchPage.class);
-            }
-        };
-        form.add(saveButton);
     }
 
     private void setupUserInterfaceFields() {
@@ -58,6 +57,45 @@ public class DonationNewPage extends BasePage {
         addTextField("suburb", new PropertyModel<String>(donationNew, "member.suburb"));
         addTextField("state", new PropertyModel<String>(donationNew, "member.state"));
 
+        Button saveButton = new Button("save") {
+            @Override
+            public void onSubmit() {
+                CollectionDatabase db =  CollectionDatabase.getInstance();
+
+                db.persist(donationNew.getDonation());
+
+                getRequestCycle().setResponsePage(DonationSearchPage.class);
+            }
+        };
+        form.add(saveButton);
+
+
+        // Category repeater
+        RepeatingView categoryListView = new RepeatingView("categoryList");
+        categoryListView.newChildId();
+    }
+
+    private void listCategories() {
+        CollectionDatabase db = CollectionDatabase.getInstance();
+        Query q = db.getEntityManager().createQuery("from Category");
+
+        List<Category> categories = q.getResultList();
+
+        availableCategories.clear();
+        for (Category category : categories) {
+            if (!categoryInDonation(category.getCode())) {
+                availableCategories.add(category);
+            }
+        }
+    }
+
+    private boolean categoryInDonation(String categoryCode) {
+        for (DonationCategory donationCategory : donationNew.getDonation().getCategoryList()) {
+            if (donationCategory.getCategoryName().equals(categoryCode)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
