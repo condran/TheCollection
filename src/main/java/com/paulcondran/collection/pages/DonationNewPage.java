14c569f8002fd430cac86ac1ae2a3abf7dd0dcb3
Paul Condran <paul@paulcondran.com>
2012-11-10 13:24:09 +1100

    DonationCategory were not linked to donation
    Signed-off-by: Paul Condran <paul@paulcondran.com>

--- src/main/java/com/paulcondran/collection/pages/DonationNewPage.java
+++ src/main/java/com/paulcondran/collection/pages/DonationNewPage.java
@@ -167,6 +167,7 @@ public class DonationNewPage extends BasePage {
 
                     if (!donationNew.getDonation().getCategoryList().contains(donationCategory)) {
                         donationNew.getDonation().getCategoryList().add(donationCategory);
+                        donationCategory.setDonation(donationNew.getDonation());
                         categoryListView.add(new CategoryPanel(categoryListView.newChildId(), null, CollectionUtil.listCategories(), this));
                         totalCategories();
                         target.add(totalField);
